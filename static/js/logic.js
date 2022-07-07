3aab8bfdb4eebf2d6971d3eb1e3d6683124a1c86
jfraz027 <jfrazier027@gmail.com>
2022-07-07 09:17:55 -0400

    update folders

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,8 +15,7 @@ function createFeatures(earthquakeData) {
   function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
   }
-
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+        // Create a GeoJSON layer that contains the features array on the earthquakeData object.
   // Run the onEachFeature function once for each piece of data in the array.
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
@@ -44,12 +43,8 @@ function createMap(earthquakes) {
 	maxZoom: 18,
 	ext: 'png'
 })
-    // // We create the dark view tile layer that will be an option for our map.
-    var CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20
-
+    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
   // Create a baseMaps object.
@@ -57,7 +52,7 @@ function createMap(earthquakes) {
     "Street Map": street,
     "Topographic Map": topo,
     "Terrain": Stamen_Terrain,
-    "Dark": CartoDB_DarkMatterNoLabels
+    "Imagery": Esri_WorldImagery
   };
 
   // Create an overlay object to hold our overlay.
@@ -74,208 +69,209 @@ function createMap(earthquakes) {
     layers: [street, earthquakes]
   });
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
+  //Create a layer control.
+  //Pass it our baseMaps and overlayMaps.
+  //Add the layer control to the map.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(myMap);
-
-  var legend = L.control({ position: 'bottomright' })
-  legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info legend')
-    var limits = [0, 1, 2, 3, 4 , 5]
-    var labels = []
+  }).addTo(myMap)};
+//}
+// Create a legend to display information about our map.
+var info = L.control({
+    position: "bottomright"
+  });
   
-    for (let i = 0; i < limits.length; i++) {
-      div.innerHTML +=
-      '<i style="background:' + colorScale(limits[i] + 1) + '"></i> ' +
-      limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');
-      }
-  
-  return div;
+  // When the layer control is added, insert a div with the class of "legend".
+  info.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
   };
-  legend.addTo(map);
+  // Add the info legend to the map.
+  info.addTo(map);
 
 
 
 
+
+
+
+
+
+// Create the tile layer that will be the background of our map.
+//var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//});
+ // Create the base layers.
+ var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+// Initialize all the LayerGroups that we'll use.
+var layers ={
+    
 }
 
 
 
+// Initialize all the LayerGroups that we'll use.
+var layers = {
+  COMING_SOON: new L.LayerGroup(),
+  EMPTY: new L.LayerGroup(),
+  LOW: new L.LayerGroup(),
+  NORMAL: new L.LayerGroup(),
+  OUT_OF_ORDER: new L.LayerGroup()
+};
+
+// Create the map with our layers.
+var map = L.map("map-id", {
+  center: [40.73, -74.0059],
+  zoom: 12,
+  layers: [
+    layers.COMING_SOON,
+    layers.EMPTY,
+    layers.LOW,
+    layers.NORMAL,
+    layers.OUT_OF_ORDER
+  ]
+});
+
+// Add our "streetmap" tile layer to the map.
+streetmap.addTo(map);
+
+// Create an overlays object to add to the layer control.
+var overlays = {
+  "Coming Soon": layers.COMING_SOON,
+  "Empty Stations": layers.EMPTY,
+  "Low Stations": layers.LOW,
+  "Healthy Stations": layers.NORMAL,
+  "Out of Order": layers.OUT_OF_ORDER
+};
+
+// Create a control for our layers, and add our overlays to it.
+L.control.layers(null, overlays).addTo(map);
+
+// Create a legend to display information about our map.
+var info = L.control({
+  position: "bottomright"
+});
+
+// When the layer control is added, insert a div with the class of "legend".
+info.onAdd = function() {
+  var div = L.DomUtil.create("div", "legend");
+  return div;
+};
+// Add the info legend to the map.
+info.addTo(map);
+
+// Initialize an object that contains icons for each layer group.
+var icons = {
+  COMING_SOON: L.ExtraMarkers.icon({
+    icon: "ion-settings",
+    iconColor: "white",
+    markerColor: "yellow",
+    shape: "star"
+  }),
+  EMPTY: L.ExtraMarkers.icon({
+    icon: "ion-android-bicycle",
+    iconColor: "white",
+    markerColor: "red",
+    shape: "circle"
+  }),
+  OUT_OF_ORDER: L.ExtraMarkers.icon({
+    icon: "ion-minus-circled",
+    iconColor: "white",
+    markerColor: "blue-dark",
+    shape: "penta"
+  }),
+  LOW: L.ExtraMarkers.icon({
+    icon: "ion-android-bicycle",
+    iconColor: "white",
+    markerColor: "orange",
+    shape: "circle"
+  }),
+  NORMAL: L.ExtraMarkers.icon({
+    icon: "ion-android-bicycle",
+    iconColor: "white",
+    markerColor: "green",
+    shape: "circle"
+  })
+};
+
+// Perform an API call to the Citi Bike station information endpoint.
+d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(function(infoRes) {
+
+  // When the first API call completes, perform another call to the Citi Bike station status endpoint.
+  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json").then(function(statusRes) {
+    var updatedAt = infoRes.last_updated;
+    var stationStatus = statusRes.data.stations;
+    var stationInfo = infoRes.data.stations;
+
+    // Create an object to keep the number of markers in each layer.
+    var stationCount = {
+      COMING_SOON: 0,
+      EMPTY: 0,
+      LOW: 0,
+      NORMAL: 0,
+      OUT_OF_ORDER: 0
+    };
+
+    // Initialize stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for the layer group.
+    var stationStatusCode;
+
+    // Loop through the stations (they're the same size and have partially matching data).
+    for (var i = 0; i < stationInfo.length; i++) {
+
+      // Create a new station object with properties of both station objects.
+      var station = Object.assign({}, stationInfo[i], stationStatus[i]);
+      // If a station is listed but not installed, it's coming soon.
+      if (!station.is_installed) {
+        stationStatusCode = "COMING_SOON";
+      }
+      // If a station has no available bikes, it's empty.
+      else if (!station.num_bikes_available) {
+        stationStatusCode = "EMPTY";
+      }
+      // If a station is installed but isn't renting, it's out of order.
+      else if (station.is_installed && !station.is_renting) {
+        stationStatusCode = "OUT_OF_ORDER";
+      }
+      // If a station has less than five bikes, it's status is low.
+      else if (station.num_bikes_available < 5) {
+        stationStatusCode = "LOW";
+      }
+      // Otherwise, the station is normal.
+      else {
+        stationStatusCode = "NORMAL";
+      }
+
+      // Update the station count.
+      stationCount[stationStatusCode]++;
+      // Create a new marker with the appropriate icon and coordinates.
+      var newMarker = L.marker([station.lat, station.lon], {
+        icon: icons[stationStatusCode]
+      });
+
+      // Add the new marker to the appropriate layer.
+      newMarker.addTo(layers[stationStatusCode]);
+
+      // Bind a popup to the marker that will  display on being clicked. This will be rendered as HTML.
+      newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
+    }
+
+    // Call the updateLegend function, which will update the legend!
+    updateLegend(updatedAt, stationCount);
+  });
+});
+
+// Update the legend's innerHTML with the last updated time and station count.
+function updateLegend(time, stationCount) {
+  document.querySelector(".legend").innerHTML = [
+    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
+    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
+    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
+    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
+    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
+  ].join("");
+}
 
 
-
-
-
-
-// // Add console.log to check to see if our code is working.
-// console.log("working");
-
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	accessToken: API_KEY
-// });
-// // We create the tile layer that will be an option for our map.
-// let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data &copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	accessToken: API_KEY
-// });
-// // We create the dark view tile layer that will be an option for our map.
-// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	accessToken: API_KEY
-// });
-
-// // Create the map object with center, zoom level and default layer.
-// let map = L.map('mapid', {
-// 	center: [39.5, -98.5],
-// zoom: 3,
-// 	layers: [streets]
-// })
-
-// // Create a base layer that holds both maps.
-// let baseMaps = {
-// 	"Streets": streets,
-//   "Satellite": satelliteStreets,
-//   "Dark": dark
-//   };
-
-// // Create the earthquake layer for our map.
-// let earthquakes = new L.LayerGroup();
-
-// // Create the tectonic layer for our map.
-// let tectonic = new L.LayerGroup();
-
-// // We define an object that contains the overlays.
-// // This overlay will be visible all the time.
-// let overlays = {
-//     Earthquakes: earthquakes,
-//     Tectonic: tectonic
-//   };
-
-// // Then we add a control to the map that will allow the user to change
-// // which layers are visible.
-// L.control.layers(baseMaps, overlays).addTo(map);
-
-// // Accessing the Tectonicplates GeoJSON URL.
-// let tectonicData = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
-
-// //Create a style for the lines.
-// let myStyle = {
-// 	color: "#ff00ff",
-// 	weight: 2
-// }
-
-// // Retrieve the tectonic GeoJSON data.
-// d3.json(tectonicData).then(function(data) {
-//   // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJson(data, {
-//     style: myStyle
-//   }).addTo(tectonic);
-      
-//       // Then we add the tectonic layer to our map.
-//       tectonic.addTo(map);
-
-//       // This function returns the style data for each of the earthquakes we plot on
-// // the map. We pass the magnitude of the earthquake into two separate functions
-// // to calculate the color and radius.
-// function styleInfo(feature) {
-//   return {
-//     opacity: 1,
-//     fillOpacity: 1,
-//     fillColor: getColor(feature.properties.mag),
-//     color: "#000000",
-//     radius: getRadius(feature.properties.mag),
-//     stroke: true,
-//     weight: 0.5
-//   };
-// }
-
-// // This function determines the color of the circle based on the magnitude of the earthquake.
-// function getColor(magnitude) {
-//   if (magnitude > 5) {
-//     return "#ea2c2c";
-//   }
-//   if (magnitude > 4) {
-//     return "#ea822c";
-//   }
-//   if (magnitude > 3) {
-//     return "#ee9c00";
-//   }
-//   if (magnitude > 2) {
-//     return "#eecc00";
-//   }
-//   if (magnitude > 1) {
-//     return "#d4ee00";
-//   }
-//   return "#98ee00";
-// }
-
-// // This function determines the radius of the earthquake marker based on its magnitude.
-// // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
-// function getRadius(magnitude) {
-//     if (magnitude === 0) {
-//         return 1;
-//       }
-//       return magnitude * 4;
-//   }
-
-// // Retrieve the earthquake GeoJSON data.
-// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-
-// // Creating a GeoJSON layer with the retrieved data.
-// L.geoJson(data, {
-//   // We turn each feature into a circleMarker on the map.
-//   pointToLayer: function(feature, latlng) {
-//       console.log(data);
-//       return L.circleMarker(latlng);
-//     },
-//   // We set the style for each circleMarker using our styleInfo function.
-// style: styleInfo,
-//   // We create a popup for each circleMarker to display the magnitude and
-//   //  location of the earthquake after the marker has been created and styled.
-//   onEachFeature: function(feature, layer) {
-//   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-// }
-// }).addTo(earthquakes);
-
-//   // Then we add the earthquake layer to our map.
-//   earthquakes.addTo(map);
-  
-  
-//   // Create a legend control object.
-//   var legend = L.control({
-//       position: 'bottomright'
-//   });
-//   // Then add all the details for the legend.
-//   legend.onAdd = function () {
-      
-//       var div = L.DomUtil.create('div', 'info legend');
-//       const magnitudes = [0, 1, 2, 3, 4, 5];
-//       const colors = [
-//           "#98ee00",
-//           "#d4ee00",
-//           "#eecc00",
-//           "#ee9c00",
-//           "#ea822c",
-//           "#ea2c2c"
-//       ];
-      
-//       // Looping through our intervals to generate a label with a colored square for each interval.
-//       for (var i = 0; i < magnitudes.length; i++) {
-//           console.log(colors[i]);
-//           div.innerHTML +=
-//           "<i style='background: " + colors[i] + "'></i> " +
-//           magnitudes[i] + (magnitudes[i + 1] ? "–" + magnitudes[i + 1] + "<br>" : "+");
-//       }
-//       return div;
-//   };
-//   legend.addTo(map);
-// });
-// });
